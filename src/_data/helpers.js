0b03bc1d4d38f10c5a49947c9d4d0fb1d5b211a3
redrubycorn <redrubycorn@gmail.com>
2023-10-12 22:14:59 +0200

    Add main navigation
    Add a mainNavigation partial and include the partial into the baseLayout.html.
    Add a navigation.json file in the _data folder and add helpers.js which includes two helper function that add the active state and aria labels to navigation links.

---
+++ src/_data/helpers.js
@@ -0,0 +1,29 @@
+module.exports = {
+    /**
+     * Returns back some attributes based on whether the
+     * link is active or a parent of an active item
+     *
+     * @param {String} itemUrl The link in question
+     * @param {String} pageUrl The page context
+     * @returns {String} The attributes or empty
+     */
+    addAriaCurrentState(itemUrl, pageUrl) {
+        let response = '';
+
+        if (itemUrl === pageUrl) {
+            response = 'aria-current="page"';
+        }
+
+        if (itemUrl.length > 1 && pageUrl.indexOf(itemUrl) === 0) {
+            response += 'data-state="active"';
+        }
+
+        return response;
+    },
+
+    addActiveState(itemUrl, pageUrl) {
+        if ((itemUrl === pageUrl) || (itemUrl.length > 1 && pageUrl.indexOf(itemUrl) === 0)) {
+            return ' active';
+        }
+    }
+};
